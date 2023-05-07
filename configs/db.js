897ebc3a1b9ff18bbab8997572e25e4c6aec52a5
Rautmahi <100849820+Rautmahi@users.noreply.github.com>
2023-05-07 19:12:45 +0530

    final data

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,21 @@
 const mongoose=require("mongoose")
+const mySecret = process.env['MONGOURL']
 require("dotenv").config()
 
-const connection=mongoose.connect(process.env.MONGOURL)
+
+// mongoose.connect(process.env.MONGOURL)
+const connection=async()=>{
+    try {
+        await mongoose.connect(mySecret, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        })
+        console.log("db connected")
+        
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
 
 module.exports={connection}
